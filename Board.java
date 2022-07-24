eb5389d264f1245cebbf05cb13ee30f8fba1868f
Kerem <93599232+Kerem0z@users.noreply.github.com>
2022-07-24 15:45:40 +0300

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,42 @@
+public class Board {
+
+
+    String[][] board;
+
+    public void initalizeBoard(String[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                matrix[i][j] = "*";
+            }
+        }
+
+    }
+
+    public void printBoard(String mat[][]) {
+
+        System.out.print(" ");
+        for (int row = 1; row < mat.length + 1; row++) {
+            System.out.print("  " + row);
+        }
+        System.out.println();
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[row].length; col++) {
+                if (col < 1) {
+                    System.out.print(row + 1);
+                    System.out.print("  " + mat[row][col]);
+                } else {
+                    System.out.print("  " + mat[row][col]);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void displayScores(int[] playerScore, int[] aiScore) {
+
+    }
+}
